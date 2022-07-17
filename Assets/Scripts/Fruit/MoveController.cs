3a2758cc592a9d38c567673d9cc70582da0d1b65
NaumovichVlad <naumovichvlad.2001@gmail.com>
2022-07-17 13:58:46 +0300

    Fruit rotation added

--- Assets/Scripts/Fruit/MoveController.cs
+++ Assets/Scripts/Fruit/MoveController.cs
@@ -7,10 +7,26 @@ public class MoveController : MonoBehaviour
     public Vector2 Direction;
 
     [SerializeField] private Vector2 attractiveForce;
+    [SerializeField] private float minRotateSpeed;
+    [SerializeField] private float maxRotateSpeed;
+
+    private float _randomRotateSpeed;
+
+    private void Start()
+    {
+        _randomRotateSpeed = GetRandomRotateSpeed();
+    }
 
     void FixedUpdate()
     {
-        transform.Translate(Direction);
-         Direction += attractiveForce;
+        transform.Translate(Direction, Space.World);
+        transform.Rotate(new Vector3(0, 0, _randomRotateSpeed));
+        Direction += attractiveForce;
+    }
+
+    private float GetRandomRotateSpeed()
+    {
+        var direction = new int[] { -1, 1 };
+        return Random.Range(minRotateSpeed, maxRotateSpeed) * direction[Random.Range(0, 1)];
     }
 }
